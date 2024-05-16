4e9677c84e1b4cdc5950e504c3b3cd1065e55098
nhatdngo <nhatdngo@gmail.com>
2024-05-16 08:20:10 -0700

    migration changes

--- backend/db/migrations/20240514220113-create-user.js
+++ backend/db/migrations/20240514220113-create-user.js
@@ -20,7 +20,7 @@ module.exports = {
         unique: true
       },
       email: {
-        type: Sequelize.STRING(256),
+        type: Sequelize.STRING(255),
         allowNull: false,
         unique: true
       },
